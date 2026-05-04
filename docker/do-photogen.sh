b71a0714cb2d4ee5631f19d6dc42060aa786b0aa
Doug Donohoe <doug@donohoe.info>
2026-05-04 13:20:33 -0400

    Use --site-id value from ddphotos in photogen; output params to photogen

--- docker/do-photogen.sh
+++ docker/do-photogen.sh
@@ -6,12 +6,23 @@ export DDPHOTOS_ALBUMS_DIR="/ddphotos/albums"
 # Relative base paths in albums.yaml are anchored to cwd; cd to the album dir so they resolve correctly.
 cd /ddphotos
 
+# Resolve env passed into container, with defaults
+CONFIG_DIR="${DDPHOTOS_CONFIG_DIR:-/ddphotos/config}"
+SITE_ID="${DDPHOTOS_SITE_ID}" # if no site-id, uses settings.id in $CONFIG_DIR/albums.yaml
+
+photogen() {
+    echo "Calling 'photogen $*' ..."
+    echo ""
+    exec /usr/local/bin/photogen "$@"
+}
+
 if [ "$1" = "--" ]; then
     shift
-    exec /usr/local/bin/photogen -config-dir "${DDPHOTOS_CONFIG_DIR:-/ddphotos/config}" "$@"
+    photogen -config-dir "$CONFIG_DIR" -site-id "$SITE_ID" "$@"
 fi
 
-exec /usr/local/bin/photogen \
-    -config-dir "${DDPHOTOS_CONFIG_DIR:-/ddphotos/config}" \
+photogen \
+    -config-dir "$CONFIG_DIR" \
+    -site-id "$SITE_ID" \
     -resize -index -clean -doit \
     "$@"
